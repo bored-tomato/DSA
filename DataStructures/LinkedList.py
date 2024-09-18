093633e3b279b9fa6589f2f91b91a8c12e6e6d5b
Jason Amaro <Jason.AmaroChiang@gmail.com>
2024-09-18 13:38:12 -0700

    Consolidate link list node into same file

--- DataStructures/LinkedList.py
+++ DataStructures/LinkedList.py
@@ -1,5 +1,13 @@
-from Node import Node
-
+class Node:
+    def __init__(self, key, val, prev = None, next = None):
+        self.key = key
+        self.val = val
+        self.prev = prev
+        self.next = next
+        
+    def __str__(self):
+        return f"({self.key}:{self.val})"
+    
 class LinkedList:
     def __init__(self, head = None):
         self.count = 0
