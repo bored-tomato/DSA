71ab7baa2f8572c2be8dde01c8fefb1934b6ece6
Jason Amaro <Jason.AmaroChiang@gmail.com>
2024-09-23 13:27:44 -0700

    Add queue implementation

--- DataStructures/Queue.py
+++ DataStructures/Queue.py
@@ -1,22 +1,25 @@
 import LinkedList
 from Node import LinkedNode
 
-class Stack:
-    def __init__(self, value=None):
+class Queue:
+    def __init__(self, value = None) -> None:
         if value: self.ll = LinkedList.LinkedList(LinkedNode(value))
         else: self.ll = LinkedList.LinkedList()
 
-    def push(self,value):
-        return self.ll.insert_at_head(LinkedNode(value))
+    def enqueue(self, value):
+        return self.ll.insert_at_end(LinkedNode(value))
 
-    def pop(self):
+    def dequeue(self):
         return self.ll.remove_from_head()
-
+    
     def peek(self):
         return self.ll.head
-
+    
+    def rear(self):
+        return self.ll.tail
+    
     def is_empty(self):
         return self.ll.is_empty()
     
     def __str__(self):
-        return f"TOP --- {self.ll} --- BOTTOM"
+        return f"FRONT -- {self.ll} -- BACK"
